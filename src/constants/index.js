3ec4824b53553944e4350ad10d05f8509e3cb3fd
Bureaucratix <bureaucratix@gmail.com>
2019-07-29 09:37:28 -0700

    updated ruby ver

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,8 @@
-export const API_ROOT = 'http://localhost:3000/api/v1';
+// export const API_ROOT = 'http://localhost:3000/api/v1';
+
+export const API_ROOT = "https://lgf-backend.herokuapp.com/api/v1";
     
 export const HEADERS = {
     'Content-Type': 'application/json',
-    Accept: 'application/json',
+    'Accept': 'application/json',
 };
